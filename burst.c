4e0cbf56a675dbf126cc1a214f2437055ebbd4c2
Matt Jackson <mcj16@uakron.edu>
2016-05-09 09:52:21 -0400

    Added pthreads and copy file functionality

--- burst.c
+++ burst.c
@@ -3,7 +3,7 @@
 	Matt Jackson
 	burst.c
 */
-
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -12,7 +12,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <pthread.h>
-#define BUF_SIZE 8192
+#define BUF_SIZE 512
 
 struct threadCopy {
 
@@ -28,6 +28,7 @@ void* copyfile_thread(void* args){
 	struct threadCopy* filecopy = args;
 	char buffer[BUF_SIZE];
 	ssize_t ret_in, ret_out;
+	printf("Thread processed\n");
 	
 		while((ret_in = read(filecopy->infd, &buffer, BUF_SIZE)) > 0){
 			ret_out = write (filecopy->outfd, &buffer, (ssize_t) ret_in);
@@ -38,37 +39,43 @@ void* copyfile_thread(void* args){
 			}	
 		}
 
-
-
 }
 int main(int argc, char* argv[]){ 
-
+	
+	int err;
 	ssize_t ret_in, ret_out;
 	char buffer[BUF_SIZE];
-
+	
 	
 	int copies = atoi(argv[3]);
 	struct threadCopy* fileCopy = calloc(copies,sizeof(struct threadCopy));
 	
 	for(int i = 0; i < copies; i++){
-	int input_fd = open(argv[1], O_RDONLY);	
+		int input_fd = open(argv[1], O_RDONLY);	
 	
-	char filename[512];
-	int test;
-	snprintf(filename, 512, "%s.%d", argv[2], i+1);
+		char filename[512];
+	
+		int test;
+	
+		snprintf(filename, 512, "%s%d.txt", argv[2], i+1);
 
-	fprintf(stderr, "%s\n", filename);
+		fprintf(stderr, "%s\n", filename);
 
-	int output_fd =open(filename,(O_WRONLY | O_CREAT), (S_IRUSR | S_IWUSR)); 
+		int output_fd =open(filename,(O_WRONLY | O_CREAT), (S_IRUSR | S_IWUSR)); 
 	
 
-	fileCopy[i].infd = input_fd;
-	fileCopy[i].outfd = output_fd;
+		fileCopy[i].infd = input_fd;
+		fileCopy[i].outfd = output_fd;
+	
+		err = pthread_create(&(fileCopy[i].tid),NULL,&copyfile_thread,&(fileCopy[i]));
 
-	int raw = pthread_create(&fileCopy[i].tid,NULL,copyfile_thread, &fileCopy[i]);
+		if(err !=0){
+			printf("\nCan't create thread: [%s]", strerror(err));
+		}
+		else{
+			printf("\nSecond thread processing\n");
+		}
 	}
-
-
 	/*
 	// open file
 	input_fd = open(argv[1], O_RDONLY);
@@ -96,5 +103,7 @@ int main(int argc, char* argv[]){
 	close(input_fd);
 	close(output_fd);
 	*/
+
+	sleep(10);
 	return 0;
 }
